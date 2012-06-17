6a0a9cf464aae2b796a27d1d7d25edeebc531364
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-17 18:31:55 +0900

    updated master_model.rb

--- app/models/library.rb
+++ app/models/library.rb
@@ -29,9 +29,7 @@ class Library < ActiveRecord::Base
   validates_uniqueness_of :short_display_name, :case_sensitive => false
   validates_uniqueness_of :isil, :allow_blank => true
   validates :display_name, :uniqueness => true
-  validates :name, :format => {:with => /^[a-z][0-9a-z]{2,254}$/}
   validates :isil, :format => {:with => /^[A-Za-z]{1,4}-[A-Za-z0-9\/:\-]{2,11}$/}, :allow_blank => true
-  #before_save :set_calil_neighborhood_library
   after_validation :geocode, :if => :address_changed?
   after_create :create_shelf
   after_save :clear_all_cache
--- spec/dummy/lib/enju_leaf/master_model.rb
+++ spec/dummy/lib/enju_leaf/master_model.rb
@@ -5,8 +5,10 @@ module MasterModel
     base.class_eval do
       acts_as_list
       validates_uniqueness_of :name, :case_sensitive => false,
-        :message => I18n.t('only_lowercase_letters_and_numbers_are_allowed')
-      validates_presence_of :name, :display_name
+        :message => I18n.t('Only lowercase letters and numbers are allowed.')
+      validates_presence_of :display_name
+      validates :name, :format => {:with => /^[a-z][0-9a-z\-_]{2,254}$/},
+        :presence => true
       before_validation :set_display_name, :on => :create
       normalize_attributes :name
     end
