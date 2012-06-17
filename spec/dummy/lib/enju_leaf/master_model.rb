52f3efdbf6673c98970b5cdd7e446cf68c7090d1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-18 00:44:31 +0900

    updated validation

--- spec/dummy/lib/enju_leaf/master_model.rb
+++ spec/dummy/lib/enju_leaf/master_model.rb
@@ -6,9 +6,8 @@ module MasterModel
       acts_as_list
       validates_uniqueness_of :name, :case_sensitive => false,
         :message => I18n.t('Only lowercase letters and numbers are allowed.')
-      validates_presence_of :display_name
-      validates :name, :format => {:with => /^[a-z][0-9a-z\-_]{2,254}$/},
-        :presence => true
+      validates :name, :presence => true
+      validates :display_name, :presence => true
       before_validation :set_display_name, :on => :create
       normalize_attributes :name
     end
