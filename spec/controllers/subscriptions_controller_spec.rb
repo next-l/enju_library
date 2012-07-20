8e777a602d5bab576b26ced08659ffc9d0e41d38
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-21 00:04:31 +0900

    added .travis.yml

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 require 'sunspot/rails/spec_helper'
 
 describe SubscriptionsController do
-  disconnect_sunspot
 
   def valid_attributes
     FactoryGirl.attributes_for(:subscription)
   end
 
-  describe "GET index" do
+  describe "GET index", :solr => true do
+    before do
+      Subscription.reindex
+    end
+
     describe "When logged in as Administrator" do
       login_admin
 
