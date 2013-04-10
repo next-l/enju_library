b5d2db6e034e52195f2479c30e0880b2d0abddd2
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-10 21:43:18 +0900

    fixed generator

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -1,7 +1,8 @@
-class EnjuLibrary::SetupGenerator < Rails::Generators::NamedBase
+class EnjuLibrary::SetupGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_library")
     rake("enju_library_engine:install:migrations")
+  end
 end
