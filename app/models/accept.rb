be526f292417ee7e8907a0184af2067d3c20cef6
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-20 01:11:16 +0900

    update use_restriction next-l/enju_leaf#198
    Conflicts:
    	app/models/accept.rb

--- app/models/accept.rb
+++ app/models/accept.rb
@@ -17,7 +17,10 @@ class Accept < ActiveRecord::Base
 
   def accept!
     if defined?(EnjuCirculation)
-      item.circulation_status = CirculationStatus.where(:name => 'Available On Shelf').first
+      circulation_status = CirculationStatus.where(:name => 'Available On Shelf').first
+      item.update_column(:circulation_status_id, circulation_status.id) if circulation_status
+      use_restriction = UseRestriction.where(name: 'Limited Circulation, Normal Loan Period').first
+      item.use_restriction = use_restriction if use_restriction
     end
     item.save(:validate => false)
   end
