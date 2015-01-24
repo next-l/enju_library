c749640fe979287b8d4067926fbfdb26553a6525
Kosuke Tanabe <nabeta@fastmail.fm>
2015-01-25 00:30:25 +0900

    added foreign_key to shelves and items

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020120523) do
+ActiveRecord::Schema.define(version: 20150124152756) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
