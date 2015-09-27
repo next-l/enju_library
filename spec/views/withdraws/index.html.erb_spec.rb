d8c17fe9fee8f4ee791cb36426eab8b942b646ae
Kosuke Tanabe <nabeta@fastmail.fm>
2015-09-28 00:54:58 +0900

    update spec files

--- spec/views/withdraws/index.html.erb_spec.rb
+++ spec/views/withdraws/index.html.erb_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "withdraws/index", type: :view do
+  fixtures :all
+
   before(:each) do
-    assign(:withdraws, [
-      FactoryGirl.create(:withdraw),
-      FactoryGirl.create(:withdraw)
-    ])
+    assign(:withdraws, Kaminari::paginate_array([
+      stub_model(Withdraw,
+        :item_id => 1,
+        :created_at => Time.zone.now
+      ),
+      stub_model(Withdraw,
+        :item_id => 1,
+        :created_at => Time.zone.now
+      )
+    ]).page(1))
+    basket = FactoryGirl.create(:basket)
+    assign(:basket, basket)
+    assign(:withdraw, basket.withdraws.new)
   end
 
   it "renders a list of withdraws" do
     render
+    assert_select "tr>td"
   end
 end
