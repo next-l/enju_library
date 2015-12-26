0882d26faa59dcb908610c7b66f130930aba48d4
Kosuke Tanabe <nabeta@fastmail.fm>
2015-12-27 00:00:22 +0900

    update spec file

--- spec/views/library_groups/edit.html.erb_spec.rb
+++ spec/views/library_groups/edit.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "library_groups/edit" do
   before(:each) do
     @library_group = LibraryGroup.site_config
+    @available_languages = Language.where(iso_639_1: I18n.available_locales.map{|l| l.to_s})
     @countries = Country.all
   end
 
