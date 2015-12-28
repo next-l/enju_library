c827645f151bfab6fc1d8a6e7921c1d0c2426236
Masao Takaku <tmasao@acm.org>
2015-12-28 21:49:51 +0900

    fix install script. cf. next-l/enju_leaf#1061

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -6,6 +6,6 @@ class EnjuLibrary::SetupGenerator < Rails::Generators::Base
     directory("db/fixtures", "db/fixtures/enju_library")
     return if file == 'fixture'
     inject_into_file 'app/controllers/application_controller.rb',
-      "  EnjuLibrary::Controller\n", after: "EnjuLeaf::Controller\n"
+      "  include EnjuLibrary::Controller\n", after: "EnjuLeaf::Controller\n"
   end
 end
