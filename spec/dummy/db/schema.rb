6909504f943b9d14133473cffde799f9fc9f6108
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-14 04:29:18 +0900

    add dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -762,8 +762,10 @@ ActiveRecord::Schema.define(version: 20160813192542) do
     t.text     "settings"
     t.text     "html_snippet"
     t.string   "email"
+    t.string   "book_jacket_source"
     t.integer  "max_number_of_results",         default: 500
     t.boolean  "family_name_first",             default: true
+    t.string   "screenshot_generator"
     t.integer  "pub_year_facet_range_interval", default: 10
   end
 
