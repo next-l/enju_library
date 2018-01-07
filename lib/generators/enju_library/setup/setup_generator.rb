f7efffaeb98defba003545fad629c69ce32b80eb
Kosuke Tanabe <nabeta@fastmail.fm>
2018-01-08 01:11:54 +0900

    remove update generator

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -8,6 +8,5 @@ class EnjuLibrary::SetupGenerator < Rails::Generators::Base
     inject_into_file 'app/controllers/application_controller.rb',
       "  include EnjuLibrary::Controller\n", after: "include EnjuLeaf::Controller\n"
     append_to_file("app/models/user.rb", "Item.include(EnjuLibrary::EnjuItem)\n")
-    generate("enju_library:update")
   end
 end
--- lib/generators/enju_library/update/update_generator.rb
+++
@@ -1,9 +0,0 @@
-class EnjuLibrary::UpdateGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  desc "Create files for updating Next-L Enju"
-
-  def copy_statesman_migration_files
-    generate('statesman:add_constraints_to_most_recent', 'UserImportFile', 'UserImportFileTransition')
-    generate('statesman:add_constraints_to_most_recent', 'UserExportFile', 'UserExportFileTransition')
-  end
-end
