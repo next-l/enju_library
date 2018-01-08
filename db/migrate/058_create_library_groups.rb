1588c932abda7a81e17c853dfc4f4c60f1b9d264
Kosuke Tanabe <nabeta@fastmail.fm>
2018-01-08 21:39:24 +0900

    revert data type of my_networks and admin_networks

--- db/migrate/058_create_library_groups.rb
+++ db/migrate/058_create_library_groups.rb
@@ -4,7 +4,7 @@ class CreateLibraryGroups < ActiveRecord::Migration[5.1]
       t.string :name, index: {unique: true}, null: false
       t.jsonb :display_name_translations
       t.string :short_name, index: {unique: true}, null: false
-      t.cidr :my_networks
+      t.text :my_networks
       t.jsonb :login_banner_translations
       t.text :note
       t.integer :country_id
