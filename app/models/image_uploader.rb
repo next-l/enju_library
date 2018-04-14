def82ae9c9eacc9a21007113826f1d03a5405547
Kosuke Tanabe <nabeta@fastmail.fm>
2018-04-14 15:53:09 +0900

    update processing option

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -10,9 +10,9 @@ class ImageUploader < Shrine
 
   process(:store) do |io, context|
     original = io.download
-    thumbnail = ImageProcessing::MiniMagick.source(original).resize_to_limit!(160, 240)
-    small = ImageProcessing::MiniMagick.source(original).resize_to_limit!(100, 150)
-    medium = ImageProcessing::MiniMagick.source(original).resize_to_limit!(640, 640)
+    thumbnail = ImageProcessing::MiniMagick.source(original).auto_orient.resize_to_limit!(160, 240)
+    small = ImageProcessing::MiniMagick.source(original).auto_orient.resize_to_limit!(100, 150)
+    medium = ImageProcessing::MiniMagick.source(original).auto_orient.resize_to_limit!(640, 640)
 
     {original: original, thumbnail: thumbnail, small: small, medium: medium}
   end
