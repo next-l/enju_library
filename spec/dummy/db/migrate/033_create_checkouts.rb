45731b4fc3eba7963ae906ed1d84e5be8c2085d5
Kosuke Tanabe <nabeta@fastmail.fm>
2018-10-30 23:52:58 +0900

    update dummy migration files

--- spec/dummy/db/migrate/033_create_checkouts.rb
+++ spec/dummy/db/migrate/033_create_checkouts.rb
@@ -1,22 +1,17 @@
 class CreateCheckouts < ActiveRecord::Migration[4.2]
   def self.up
     create_table :checkouts do |t|
-      t.integer :user_id
-      t.integer :item_id, null: false
-      t.integer :checkin_id
-      t.integer :librarian_id
-      t.integer :basket_id
+      t.references :user, index: true, foreign_key: true
+      t.references :item, index: true, foreign_key: true, null: false
+      t.references :checkin, index: true, foreign_key: true
+      t.references :librarian, index: true
+      t.references :basket, index: true
       t.datetime :due_date
       t.integer :checkout_renewal_count, default: 0, null: false
       t.integer :lock_version, default: 0, null: false
       t.timestamps
     end
-    add_index :checkouts, :user_id
-    add_index :checkouts, :item_id
-    add_index :checkouts, :basket_id
     add_index :checkouts, [:item_id, :basket_id], unique: true
-    add_index :checkouts, :librarian_id
-    add_index :checkouts, :checkin_id
   end
 
   def self.down
