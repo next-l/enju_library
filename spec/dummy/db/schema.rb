a06bd2afe8829b51b071016260ca6bfa3f5d191b
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-13 00:43:20 +0900

    update dummy migration file

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -542,20 +542,6 @@ ActiveRecord::Schema.define(version: 2019_02_08_135957) do
     t.index ["name"], name: "index_languages_on_name", unique: true
   end
 
-  create_table "lending_policies", force: :cascade do |t|
-    t.uuid "item_id", null: false
-    t.uuid "user_group_id", null: false
-    t.integer "loan_period", default: 0, null: false
-    t.datetime "fixed_due_date"
-    t.integer "renewal", default: 0, null: false
-    t.integer "fine", default: 0, null: false
-    t.text "note"
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id", "user_group_id"], name: "index_lending_policies_on_item_id_and_user_group_id", unique: true
-  end
-
   create_table "libraries", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.jsonb "display_name_translations", default: {}, null: false
@@ -1277,8 +1263,6 @@ ActiveRecord::Schema.define(version: 2019_02_08_135957) do
   add_foreign_key "items", "bookstores"
   add_foreign_key "items", "manifestations"
   add_foreign_key "jpno_records", "manifestations"
-  add_foreign_key "lending_policies", "items"
-  add_foreign_key "lending_policies", "user_groups"
   add_foreign_key "libraries", "library_groups"
   add_foreign_key "library_groups", "users"
   add_foreign_key "manifestation_relationships", "manifestations", column: "child_id"
