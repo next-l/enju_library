7ebb348902f4382bfd11b279ceb9969352acbde9
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-30 20:33:30 +0900

    update schema.rb

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107160740) do
+ActiveRecord::Schema.define(version: 2018_01_07_160740) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
