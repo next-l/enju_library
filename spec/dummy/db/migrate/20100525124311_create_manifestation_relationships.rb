88bd54367c0ff553bfa91b22fd87d772addd24b1
Kosuke Tanabe <nabeta@fastmail.fm>
2019-05-11 13:29:51 +0900

    update dummy migration files

--- spec/dummy/db/migrate/20100525124311_create_manifestation_relationships.rb
+++ spec/dummy/db/migrate/20100525124311_create_manifestation_relationships.rb
@@ -3,7 +3,7 @@ class CreateManifestationRelationships < ActiveRecord::Migration[5.2]
     create_table :manifestation_relationships do |t|
       t.references :parent, foreign_key: {to_table: :manifestations}, null: false
       t.references :child, foreign_key: {to_table: :manifestations}, null: false
-      t.references :manifestation_relationship_type, index: false
+      t.references :manifestation_relationship_type, index: false, null: false
 
       t.timestamps
     end
--- spec/dummy/db/migrate/20111124110059_create_create_types.rb
+++ spec/dummy/db/migrate/20111124110059_create_create_types.rb
@@ -1,10 +1,10 @@
 class CreateCreateTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :create_types do |t|
-      t.string :name
+      t.string :name, null: false
       t.jsonb :display_name_translations, default: {}, null: false
       t.text :note
-      t.integer :position
+      t.integer :position, default: 1, null: false
 
       t.timestamps
     end
