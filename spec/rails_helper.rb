5ab64b57646ed1f5d7bd26ff5dd6d49662824324
Kosuke Tanabe <nabeta@fastmail.fm>
2019-07-11 23:29:09 +0900

    set simplecov formatter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start 'rails'
-Coveralls.wear!
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
