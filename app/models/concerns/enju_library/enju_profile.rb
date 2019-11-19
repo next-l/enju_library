5c49db01d55bb3dc76a0105e9bc3f1c70ee6b9fc
Kosuke Tanabe <nabeta@fastmail.fm>
2019-11-19 19:56:25 +0900

    add user_group_id and library_id to profiles

---
+++ app/models/concerns/enju_library/enju_profile.rb
@@ -0,0 +1,22 @@
+module EnjuLibrary
+  module EnjuProfile
+    extend ActiveSupport::Concern
+
+    included do
+      belongs_to :library
+      belongs_to :user_group
+      validates_associated :user_group, :library
+      validates_presence_of :user_group, :library, :locale #, :user_number
+    end
+
+    # ユーザの有効期限を設定します。
+    # @return [Time]
+    def set_expired_at
+      if expired_at.blank?
+        if user_group.valid_period_for_new_user > 0
+          self.expired_at = user_group.valid_period_for_new_user.days.from_now.end_of_day
+        end
+      end
+    end
+  end
+end
