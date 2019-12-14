f85c58e60aea640756ab9d0ce7a1d998ffb623bf
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-14 16:39:46 +0900

    update generator

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -5,8 +5,8 @@ class EnjuLibrary::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_library")
     return if file == 'fixture'
-    inject_into_file 'app/controllers/application_controller.rb',
-      "  include EnjuLibrary::Controller\n", after: "include EnjuLeaf::Controller\n"
+    inject_into_class 'app/controllers/application_controller.rb', ApplicationController,
+      "  include EnjuLibrary::Controller\n"
     append_to_file "app/models/user.rb", <<EOS
 Item.include(EnjuLibrary::EnjuItem)
 Profile.include(EnjuLibrary::EnjuProfile)
