25c2931ff8ff92edfff14f8e5526e8e226a74ad6
Kosuke Tanabe <nabeta@fastmail.fm>
2019-12-30 21:15:15 +0900

    Revert "update generator"
    This reverts commit 4bafaece21980b8ab93a69e5ffe5973ac80609f9.

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -8,10 +8,5 @@ class EnjuLibrary::SetupGenerator < Rails::Generators::Base
     inject_into_class 'app/controllers/application_controller.rb', ApplicationController,
       "  include EnjuLibrary::Controller\n"
     append_to_file("app/models/user.rb", "Item.include(EnjuLibrary::EnjuItem)\n")
-    gsub_file 'app/models/user.rb', /, :validatable$/, <<EOS
-, # :validatable,
-      :lockable, lock_strategy: :none, unlock_strategy: :none
-  include EnjuSeed::EnjuUser
-EOS
   end
 end
