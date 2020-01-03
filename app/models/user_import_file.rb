54d8fb5130d1d92b2687664e2e4d752464d5d3d5
Kosuke Tanabe <nabeta@fastmail.fm>
2020-01-03 18:26:14 +0900

    clean up

--- app/models/user_import_file.rb
+++ app/models/user_import_file.rb
@@ -62,14 +62,14 @@ class UserImportFile < ApplicationRecord
       next if row['dummy'].to_s.strip.present?
 
       username = row['username']
-      new_user = User.where(username: username).first
+      new_user = User.find_by(username: username)
       if new_user
         import_result.user = new_user
         import_result.save
         num[:user_found] += 1
       else
         new_user = User.new
-        new_user.role = Role.where(name: row['role']).first
+        new_user.role = Role.find_by(name: row['role'])
         if new_user.role
           unless user.has_role?(new_user.role.name)
             num[:failed] += 1
@@ -145,7 +145,7 @@ class UserImportFile < ApplicationRecord
       )
 
       username = row['username']
-      new_user = User.where(username: username).first
+      new_user = User.find_by(username: username)
       if new_user.try(:profile)
         new_user.assign_attributes(set_user_params(row))
         new_user.profile.assign_attributes(set_profile_params(row))
@@ -288,15 +288,15 @@ class UserImportFile < ApplicationRecord
   # @param [Hash] row 利用者情報のハッシュ
   def set_profile_params(row)
     params = {}
-    user_group = UserGroup.where(name: row['user_group']).first
+    user_group = UserGroup.find_by(name: row['user_group'])
     unless user_group
       user_group = default_user_group
     end
     params[:user_group_id] = user_group.id if user_group
 
-    required_role = Role.where(name: row['required_role']).first
+    required_role = Role.find_by(name: row['required_role'])
     unless required_role
-      required_role = Role.where(name: 'Librarian').first
+      required_role = Role.find_by(name: 'Librarian')
     end
     params[:required_role_id] = required_role.id if required_role
 
@@ -318,7 +318,7 @@ class UserImportFile < ApplicationRecord
       params[:locale] = row['locale']
     end
 
-    library = Library.where(name: row['library'].to_s.strip).first
+    library = Library.find_by(name: row['library'].to_s.strip)
     unless library
       library = default_library || Library.web
     end
