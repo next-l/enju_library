ecea4fa310317dcf5f6666344ffffb00d5fad540
Kosuke Tanabe <nabeta@fastmail.fm>
2020-01-10 00:18:30 +0900

    update generator

--- lib/generators/enju_library/setup/setup_generator.rb
+++ lib/generators/enju_library/setup/setup_generator.rb
@@ -7,6 +7,8 @@ class EnjuLibrary::SetupGenerator < Rails::Generators::Base
     return if file == 'fixture'
     inject_into_class 'app/controllers/application_controller.rb', ApplicationController,
       "  include EnjuLibrary::Controller\n"
+    inject_into_class 'app/models/user.rb', User,
+      "  include EnjuLibrary::EnjuUser\n"
     append_to_file "app/models/user.rb", <<EOS
 Item.include(EnjuLibrary::EnjuItem)
 Profile.include(EnjuLibrary::EnjuProfile)
