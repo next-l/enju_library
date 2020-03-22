5431e3c10027cb522ccdb3f351c449dd613a1c7f
Kosuke Tanabe <nabeta@fastmail.fm>
2020-03-22 14:57:39 +0900

    add default_custom_manifestation_label and default_custom_item_label

--- app/models/library_group.rb
+++ app/models/library_group.rb
@@ -6,8 +6,6 @@ class LibraryGroup < ApplicationRecord
   has_many :colors, dependent: :destroy
   belongs_to :country, optional: true
   belongs_to :user, optional: true
-  has_many :default_custom_manifestation_labels, dependent: :destroy
-  has_many :default_custom_item_labels, dependent: :destroy
 
   validates :url, presence: true, url: true
   validates :max_number_of_results, numericality: {
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_122214) do
+ActiveRecord::Schema.define(version: 2020_03_22_053113) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
